657a69a1c178761f8b940eb46ef9154f9c7c1873
Vincent Dagpin <vincent.dagpin@gmail.com>
2021-02-14 20:27:11 +0800

    Init 2

---
+++ Lynx.MobileApp/Lynx.MobileApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Lynx.MobileApp.Services;
+using Lynx.MobileApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lynx.MobileApp
+{
+    public partial class App : Xamarin.Forms.Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Lynx.MobileApp/Lynx.MobileApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Lynx.MobileApp.ViewModels;
+using Lynx.MobileApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Lynx.MobileApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
